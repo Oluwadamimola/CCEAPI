ccfcedb75f4d05c237e0c24245d7bb34812fea4e
Oluwadamimola <damimolaadedeji@gmail.com>
2025-10-29 09:42:02 +0100

    INITIATE

--- Services/ImageService.cs
+++ Services/ImageService.cs
@@ -17,7 +17,6 @@ namespace CCEAPI.Services
     public class ImageService : IImageService
     {
         private readonly AppDbContext _context;
-        private readonly string _imagePath;
 
         public ImageService(AppDbContext context)
         {
